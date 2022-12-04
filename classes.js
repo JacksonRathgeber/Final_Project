c25b8edb14d9b42471c1849f90eb859ca2a45476
JacksonRathgeber <jr5348@nyu.edu>
2022-12-03 22:54:59 -0500

    hand detection implemented

--- classes.js
+++ classes.js
@@ -5,12 +5,16 @@ class Thing {
 		this.yspeed=0;
 		this.xspeed=0;
 		this.size=.05;
+		this.x=0;
+		this.xspeed=1;
 	}
 
 	update(){
 		this.size*=1.05;
 		this.y+=this.yspeed;
-		this.yspeed+=0.25;
+		this.yspeed+=0.01;
+		this.x+=this.xspeed;
+		this.xspeed+=0.01;
 	}
 
 
@@ -26,8 +30,6 @@ class Obstacle extends Thing {
 class Bear extends Obstacle {
 	constructor(x_){
 		super(x_);
-		this.x=0;
-		this.xspeed=1;
 	}
 
 	display(){
@@ -35,12 +37,42 @@ class Bear extends Obstacle {
 		rect(this.x,this.y,this.size*2,this.size);
 	}
 
-	update(){
-		this.size*=1.05;
-		this.y+=this.yspeed;
-		this.yspeed+=0.01;
-		this.x+=this.xspeed;
-		this.xspeed+=0.01;
+}
+
+
+class Car {
+	constructor(){
+		this.palmLoc=createVector(0,0);
+		this.predictions=[];
 	}
 
+	display(){
+		fill(255,204,0);
+  		stroke(255,150,0);
+  		rect(this.palmLoc.x,this.palmLoc.y,200,50,10);
+	}
+
+
+	update(){
+		//console.log("running");
+	    this.palmLoc.set(0,0);
+	  
+	    for (let i = 0; i < this.predictions.length; i += 1) {
+	        const prediction = this.predictions[i];
+	        for (let j = 0; j < prediction.landmarks.length; j += 1) {
+	          const keypoint = prediction.landmarks[j];
+	          this.palmLoc.add(floor(keypoint[0]),floor(keypoint[1]));
+	          //console.log(keypoint[0],keypoint[1]);
+	          //ellipse(keypoint[0], keypoint[1], 10, 10);
+	        }
+	      }
+	    this.palmLoc.div(this.predictions.length*20);
+	    this.palmLoc.x=map(this.palmLoc.x,180,500,width*7/8,width/8,true);
+	    this.palmLoc.y=map(this.palmLoc.y,220,380,height*3/4,height,true);
+	}
+
+
+	getData(data){
+		this.predictions=data;
+	}
 }
--- sketch.js
+++ sketch.js
@@ -1,17 +1,41 @@
-let testBear;
+let video;
+let handpose;
+let car;
+
 
 function setup() {
-  createCanvas(windowWidth,windowHeight);
-  testBear=new Bear();
+  createCanvas(900,680);
+  video=createCapture(VIDEO);
+  video.hide();
+
+  car=new Car();
+
+  handpose = ml5.handpose(video);
+  
+  handpose.on("predict", results => {
+    car.getData(results);
+  });
+
+  console.log("width: "+windowWidth+", height: "+windowHeight);
 }
 
 function draw() {
   background(150,200,255);
+
+  if(car.predictions.length>0){
+    car.update()
+      
+    } else{
+      //console.log("not running");
+    }
+
   noStroke();
   fill(75,125,75);
   rect(0,height/2,width,height/2);
   fill(60);
   triangle(width/2,height/2,width/8,height,7*width/8,height);
-  testBear.display();
-  testBear.update();
+
+  car.display();
+  
 }
+
