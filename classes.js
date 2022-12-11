8114e55d7f21aec233d5f1e00742191a825095d1
JacksonRathgeber <jr5348@nyu.edu>
2022-12-11 01:45:53 -0500

    p5.play integration + collision fix

--- classes.js
+++ classes.js
@@ -3,7 +3,7 @@
 //class inheritance, subclasses, etc. done with help of p5.js reference
 // ^^ https://p5js.org/examples/objects-inheritance.html
 
-class Thing {
+class Thing { // all-inclusive object for everything but player's car
 	constructor(mode){
 		this.x=mode*width;
 		this.y=height/2;
@@ -16,9 +16,11 @@ class Thing {
 		this.mode=mode;
 		this.yInc=1;
 		this.xInc=0.01;
+
+		// properties detail how the object moves, grows, etc.
 	}
 
-	update(){
+	update(){ // move object down and increase size to mimic coming closer in perspective
 		this.size+=this.sizeInc;
 		this.y+=this.yspeed;
 		this.yspeed+=this.yInc;
@@ -27,7 +29,10 @@ class Thing {
 		//console.log("x: "+this.x+", y: "+this.y);
 	}
 
-	move(){
+	move(){ // move object laterally across screen, accelerating over time
+
+		// object moves either left or right depending on mode
+
 		if(this.mode==0){
 			this.x+=this.xspeed;
 			this.xspeed+=this.xInc;
@@ -47,14 +52,23 @@ class Thing {
 }
 
 
-class RoadLine extends Thing {
+class RoadLine extends Thing { // subclass of Thing
 	constructor(x_){
 		super(x_);
 		this.x=width/2;
 		this.y=height/2;
 	}
 
-	display(){
+	display(){ 
+		// draw trapezoid shape, grow and move down over time
+		// works hand-in-hand with superclass update() function
+		
+		/*
+		let roadLine=new Sprite([[this.x+this.size*-0.25,this.y],[this.x+this.size*.25,this.y],
+			[this.x+this.size*0.5,this.y+this.size*1.5], [this.x+this.size*-0.5,this.y+this.size*1.5]]);
+		roadLine.color=color(255,204,0);
+		roadLine.collider='none';
+		*/
 		fill(255,204,0);
 		beginShape();
 		vertex(this.x+this.size*-0.25,this.y);
@@ -67,27 +81,36 @@ class RoadLine extends Thing {
 }
 
 
-class Obstacle extends Thing {
+class Obstacle extends Thing { // subclass of Thing, envelops all obstacles
 	constructor(x_, mode){
-		super(x_, mode);
+		super(x_, mode); //references Thing library for x_ and mode
 		this.xSize=0;
 		this.ySize=0;
 	}
 
 }
 
-class Bear extends Obstacle {
+
+/*
+class Bear extends Obstacle { 
+
+	//specific subclass within obstacles
+	//most basic obstacle, simple rectangle nicknamed "Bear"
+
 	constructor(x_, mode, yInc, sizeInc, sizeInc2, xInc){
-		super(x_, mode, yInc, sizeInc, sizeInc2, xInc);
+		super(x_, mode, yInc, sizeInc, sizeInc2, xInc); //references superclass for variables
 		this.yInc=random(0.025, 0.250);
 		this.xSize=2*this.size;
 		this.ySize=this.size;
 		this.sizeInc=0.025;
 		this.sizeInc2=0.025;
 		this.xInc=random(0,0.5);
+
+		// redefine values for variables controlling movement
+		// some numbers are random to make things more interesting
 	}
 
-	display(){
+	display(){ // show bear as simple rectangle
 		fill(255,0,0);
 		
 		this.xSize=2*this.size;
@@ -97,47 +120,48 @@ class Bear extends Obstacle {
 
 	}
 
-		
-
 }
 
 
 
-class Car {
+class Car { //player avatar
 	constructor(){
-		this.palmLoc=createVector(0,0);
-		this.predictions=[];
+		this.palmLoc=createVector(0,0); //represents hand position
+		this.predictions=[]; // store position data
 		this.xSize=100;
 		this.ySize=25;
 	}
 
-	display(){
+	display(){ //draw simple rectangle for car
 		fill(80,100,255);
   		noStroke();
   		rect(this.palmLoc.x,this.palmLoc.y,this.xSize,this.ySize,10);
 	}
 
 
-	update(){
+	update(){ //move car to hand position
 		//console.log("running");
 	    this.palmLoc.set(0,0);
 	  
-	    for (let i = 0; i < this.predictions.length; i += 1) {
+	    for (let i = 0; i < this.predictions.length; i += 1) { 
+
+			// iterate over predictions
+			// (there's actually only ever one prediction in the array, so this is uneccesary)
+			// if you're reading this, I'll fix it later don't worry
+
 	        const prediction = this.predictions[i];
 	        for (let j = 0; j < prediction.landmarks.length; j += 1) {
 	          const keypoint = prediction.landmarks[j];
 	          this.palmLoc.add(floor(keypoint[0]),floor(keypoint[1]));
-	          //console.log(keypoint[0],keypoint[1]);
+
 	          //ellipse(keypoint[0], keypoint[1], 10, 10);
 	        }
 	      }
 	    this.palmLoc.div(this.predictions.length*20);
 	    this.palmLoc.x=map(this.palmLoc.x,180,500,width*7/8,width/8,true);
 	    this.palmLoc.y=map(this.palmLoc.y,220,380,height*3/4,height,true);
-	}
 
-
-	getData(data){
-		this.predictions=data;
+	    // ^^ map hand position to stick within box at bottom of screen
 	}
 }
+*/
--- sketch.js
+++ sketch.js
@@ -9,13 +9,22 @@ let timer;
 let car;
 let gameStarted;
 let roadLines;
-let key;
 let finalScore;
-let gameEnded;
+let predictions;
+let carLoc;
 
 let bears;
+let bear;
 
 function setup() {
+  
+  roadLines=[];
+
+  /*
+  bears=new Group();
+  bears.color='red';
+  */
+
   createCanvas(900,680);
   video=createCapture(VIDEO);
   video.hide();
@@ -23,18 +32,29 @@ function setup() {
   textSize(24);
   textAlign(CENTER);
 
-  car=new Car();
+  predictions=[];
 
-  roadLines=[];
-  bears=[];
+  car=new Sprite(0,0,100,25);
+  car.color=color(80,100,255);
+
+  carLoc=createVector(0,0);
+
+
+  bears=new Group();
+  bears.color='red';
+  bears.y=height/2;
+  bears.width=1.5;
+  bears.height=0.75;
+  //bears.overlaps(bears);
+  //bears.overlaps(car);
 
   gameStarted=false;
   gameEnded=false;
 
-  handpose = ml5.handpose(video);
+  handpose = ml5.handpose(video, gameStart);
   
   handpose.on("predict", results => {
-    car.getData(results);
+    getData(results);
   });
 
   //console.log("width: "+windowWidth+", height: "+windowHeight);
@@ -44,17 +64,27 @@ function draw() {
   background(150,200,255);
   timer=round(millis()/1000,2);
 
-  if(round(timer%2,1)<=0.2 && gameStarted==true){
-    let line=new RoadLine();
-    roadLines.push(line);
+  if(round(timer%2,1)<=0.1 && gameStarted==true){
+    let roadLine=new RoadLine();
+    roadLines.push(roadLine);
+    console.log("Roadline created");
   }
 
-  if((round(timer%5,1)<=0.15 || random(200)==69) && gameStarted==true){
-    key=round(random(0,1));
-    let bear=new Bear(key);
-    bears.push(bear);
 
-    //console.log(key);
+  if((round(random(125))==4 || round(timer%2,1)<=0.1) && gameStarted==true){
+    bear=new bears.Sprite();
+        
+    bear.overlaps(bears);
+
+    bear.x = width*round(random(0,1));
+    bear.vel.y = random(0.1,0.75);
+    if(bear.x==0){
+      bear.vel.x = random(0.1,0.75);
+    }
+    else if(bear.x==width){
+      bear.vel.x  = random(-0.75,-0.1);
+    }
+    console.log("Bear created");
   }
 
 
@@ -63,16 +93,17 @@ function draw() {
     text("Loading...",width/2,150);
   }
 
-  if (car.predictions.length>0){
-    if(gameStarted==false){
-      gameStart();
-    }
-    car.update()
+  if (predictions.length>0){
+    updateCar(car);
+    /*
+    car.x=carLoc.x;
+    car.y=carLoc.y;
+    */
       
   } else{
       if(gameStarted==true){
         fill(255,0,0);
-        text("No hand detected.",width/2,150);
+        text("Move hand into frame",width/2,150);
       }
   }
 
@@ -81,7 +112,7 @@ function draw() {
   rect(0,height/2,width,height/2);
   fill(60);
   triangle(width/2,height/2,width/8,height,7*width/8,height);
-
+    
   
   for(let i=0;i<roadLines.length;i++){
     roadLines[i].display();
@@ -90,36 +121,28 @@ function draw() {
     if(roadLines[i].y>height){
       roadLines.splice(roadLines[i],1);
     }
+
   }
 
-  for(let i=0;i<bears.length;i++){
-    bears[i].display();
-    bears[i].update();
-    bears[i].move();
 
-    //console.log(bears[i].mode);
-
-    let bearX=int(bears[i].x);
-    let bearY=int(bears[i].y);
-
+  for (let i=0;i<bears.length;i++){
+    bears[i].vel.y*=1.11;
+    bears[i].vel.x*=1.11;
+    bears[i].width*=1.11;
+    bears[i].height*=1.11;
     
-    if(bears.length>=1){
-      if(abs(car.palmLoc.x-bearX)<bears[i].xSize*map(bears[i].mode,0,1,1,0)+car.xSize &&
-      abs(car.palmLoc.y-bearY)<bears[i].ySize+car.ySize){
-        console.log("Collision!");
-        gameEnded=true;
-        finalScore=round(millis()/10000);
-      }
+    if(car.overlapping(bears[i])){
+      console.log("Game Over!");
+      gameEnd();
     }
-    if(bears[i].y>=height){
-      bears.splice(i,1);
+
+    if(bears[i].y>height){
+      bears[i].remove();
     }
+
   }
-    
-  if(gameEnded==true){
-    gameEnd();
-  }
-  car.display();
+  
+
 }
 
 
@@ -138,8 +161,44 @@ function gameEnd(){
     textSize(50);
     text("Game Over!",width/2, 150);
     textSize(18);
-    text("Score: "+finalScore,width/2, 250);
+    text("Score: "+floor(timer/2)-8,width/2, 250);
     noLoop();
+}
+
+
+
+
+function getData(data){
+    predictions=data;
+}
+
+
+
+
+function updateCar(str){ //move car to hand position
+//console.log("running");
+  carLoc.set(0,0);
+
+  for (let i = 0; i < predictions.length; i += 1){  
+    const prediction=predictions[i];
+    for (let j = 0; j < prediction.landmarks.length; j += 1) {
+      const keypoint = prediction.landmarks[j];
+      carLoc.add(floor(keypoint[0]),floor(keypoint[1]));
+      //console.log("x: "+floor(keypoint[0])+", y: "+floor(keypoint[1]));
+
+      //ellipse(keypoint[0], keypoint[1], 10, 10);
+    }
+  }
+
+  carLoc.div(20);
+  carLoc.x=map(carLoc.x,180,500,width*7/8,width/8,true);
+  carLoc.y=map(carLoc.y,220,380,height*3/4,height,true);
+
+  // ^^ map hand position to stick within box at bottom of screen
+
+  str.x=carLoc.x;
+  str.y=carLoc.y;
+
 }
 
 
